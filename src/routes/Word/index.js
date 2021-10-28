924f793169731a74cc1c4c636968dd3129cd5d4e
corin22 <qwg6904@gmail.com>
2021-10-28 12:25:36 +0900

    index.js

--- src/routes/Word/index.js
+++ src/routes/Word/index.js
@@ -3,18 +3,45 @@ const WordRouter = express.Router()
 
 const Word = require('../../models/Word')
 
-WordRouter.route('/(:word)?').get(async (req, res) => {
+WordRouter.route('/(:word)?').get( async (req, res) => {
     let words = []
     const { word } = req.params
+    const queries = word.split(',')
+    console.log(queries)
 
     if(word !== "undefined" && word !== undefined){
-        console.log(word)
-        words = await Word.find({ r_word: word})
+        console.log(queries)
+        try{
+            console.log('단어 쿼리...')
+            // words = await Word.find({ r_des: {$in: [
+            //     {$regex: "법규"},
+            //     {$regex: "계속"}
+            // ]}})
+            // words = await Word.find({ r_word: word}) // 데이터베이스에서 쿼리로 단어를 검색
+            // words = await Word.find({ r_word: { $regex: `^${word}`}}) // 데이터베이스에서 검색어로 시작하는 단어를 검색
+            // words = await Word.find({ r_word: { $regex: `${word}$`}}) // 데이터베이스에서 검색어로 끝나는 단어를 검색
+            // words = await Word.find({ r_des: { $regex: `${word}`}}) // 
+
+            words = await Word.find({ 
+                $or: [
+                    {r_word: {$regex: `${word}`}},
+                    {r_des: {$regex: `${word}`}}
+                ]
+            }).sort({"_id": -1})
+            // .limit(6) // -1 : 최신순(내림차순), 1: 과거순(오름차순)
+        }catch(e){
+            console.log(e)
+        }
+        
     }else{
         console.log(word)
-        // console.log(`word database: ${Word}`)
+        console.log(`word database: ${Word}`)
         // 데이터베이스에서 전체 단어 검색
-        words = await Word.find()
+        try{
+            words = await Word.find()
+        }catch(e){
+            console.log(e)
+        }
         
     }
     res.json({status: 200, words})
